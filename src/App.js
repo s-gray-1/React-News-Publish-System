7e72d66a5113d4f8a1b7aeb0c26c1ed14230ab36
s-gray-1 <s_gray@foxmail.com>
2022-03-16 14:21:45 +0800

    Json-server

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React,{useEffect} from 'react'
+import IndexRouter from './router/IndexRouter';
+import './App.css'
+import { Provider } from 'react-redux';
+import {store,persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+export default function App() {
+  useEffect(() => {
+    
+  
+  }, []);
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <IndexRouter></IndexRouter>
+      </PersistGate>
+      
+    </Provider>
+      
+   
+  )
+}
